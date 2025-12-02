9728ffbd649f83fb406fcce2b280a7643c8dfa08
Anderson Ramirez <ar2527@cornell.edu>
2025-12-02 16:10:40 -0500

    added xcode project

---
+++ cs1998-hackathon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cs1998-hackathon
+//
+//  Created by Anderson Ramirez on 12/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cs1998-hackathon/cs1998_hackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  cs1998_hackathonApp.swift
+//  cs1998-hackathon
+//
+//  Created by Anderson Ramirez on 12/2/25.
+//
+
+import SwiftUI
+
+@main
+struct cs1998_hackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
